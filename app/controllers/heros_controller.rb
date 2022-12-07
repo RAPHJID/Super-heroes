723730e0f602ce9a97d1b2466313fd9c190af6bf
Jid <raphjid170@gmail.com>
2022-12-08 01:04:40 +0300

    routes related to heroes controller

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,18 @@
 class HerosController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound,with: :unfound_response 
+        def index
+            render json: Hero.all, status: :ok
+        end
+    
+        def show
+            hero = Hero.find(params[:id])
+            render json: hero,serializer: HeroheroSerializer, status: :ok
+    
+        end
+    
+        private
+    
+        def unfound_response
+            render json: {error: "Hero not found"},status: :not_found
+        end
+    end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,13 @@
 class PowersController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound,with: :unfound_response 
+        def show
+            power = Power.find(params[:id])
+            render json: power,status: :ok
+        end
+    
+        private
+    
+        def unfound_response
+            render json: {error: "Power not found"}
+        end
+    end
