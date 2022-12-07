8f130c3f69145b0cbadd507d4e605e6c7df0ebfe
Jid <raphjid170@gmail.com>
2022-12-08 01:08:21 +0300

    power controller updated

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,13 +1,32 @@
 class PowersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound,with: :unfound_response 
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+    
+        def index
+            render json: Power.all,status: :ok
+        end
+    
         def show
             power = Power.find(params[:id])
             render json: power,status: :ok
         end
     
+        def update
+            power = Power.find_by(id: params[:id])
+            power.update!(power_params)
+            render json: power,status: :accepted
+        end
         private
     
         def unfound_response
             render json: {error: "Power not found"}
+            render json: {error: "Power not found"},status: :not_found
+        end
+    
+        def invalid_record
+            render json:{errors: "[validation errors]"},status: :unprocessable_entity
+        end
+        def power_params
+            params.permit(:description)
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :hero_powers
-  resources :powers
+  resources :powers,only:[:show,:index,:update]
   resources :heros,only: [:show, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
