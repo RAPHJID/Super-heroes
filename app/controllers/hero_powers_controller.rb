3db0fdb5e9605982afcf7e3ac3451450b6b27512
Jid <raphjid170@gmail.com>
2022-12-08 01:23:28 +0300

    updated hero power controller

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,13 +1,20 @@
 class HeroPowersController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid,  with: :invalid_record
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+    rescue_from ActiveRecord::RecordNotFound, with: :invalid_record
         def create
-            heropower = HeroPower.create!(heropower_params)
+            heropower = HeroPower.new(heropower_params)
             hero = Hero.find(heropower.hero_id)
-            render json: hero,serializer: HeroheroSerializer,status: :created
+            power = Power.find(heropower.power_id)
+            if hero && power
+                heropower = HeroPower.create!(heropower_params)
+                render json: hero,serializer: HeroheroSerializer,status: :created
+            else
+                invalid_record
+            end
         end
     
         private
-    
+        
         def heropower_params
            params.permit(:strength, :power_id,:hero_id)
         end
